23bde11ebd3db42ef1c3d19426afce9dc53b6fef
Benjamin <ayc0.benj@gmail.com>
2025-05-23 17:59:10 +0200

    feat!: update useEffectEvent to be a better match for the native implementation (#22)
    * Update useEffectEvent.ts
    
    * Update src/useEffectEvent.ts
    
    * chore(eslint): enable reportUnusedDisableDirectives and remove non-required eslint-disable
    
    * chore(test): add vitest + RTL + add tests for edge cases
    
    * chore(eslint): use experimental build of eslint-plugin-react-hooks

--- src/useEffectEvent.ts
+++ src/useEffectEvent.ts
@@ -1,4 +1,8 @@
-import {useCallback, useInsertionEffect, useRef} from 'react'
+import {useInsertionEffect, useRef} from 'react'
+
+function forbiddenInRender() {
+  throw new Error("A function wrapped in useEffectEvent can't be called during rendering.")
+}
 
 /**
  * This is a ponyfill of the upcoming `useEffectEvent` hook that'll arrive in React 19.
@@ -6,19 +10,16 @@ import {useCallback, useInsertionEffect, useRef} from 'react'
  * To learn more about the ponyfill itself, see: https://blog.bitsrc.io/a-look-inside-the-useevent-polyfill-from-the-new-react-docs-d1c4739e8072
  * @public
  */
-export function useEffectEvent<
-  const T extends (
-    ...args: // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    any[]
-  ) => void,
->(fn: T): T {
+export function useEffectEvent<const T extends (...args: any[]) => void>(fn: T): T {
   const ref = useRef<T | null>(null)
+  ref.current = forbiddenInRender as T
+
   useInsertionEffect(() => {
     ref.current = fn
   }, [fn])
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return useCallback((...args: any) => {
+
+  return ((...args: any) => {
     const latestFn = ref.current!
     return latestFn(...args)
-  }, []) as unknown as T
+  }) as T
 }
